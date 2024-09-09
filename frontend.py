116bf52f516f4a6b253986b24e08c0a5f72d84f5
BrianSalk <brian.salkas@gmail.com>
2024-09-09 12:34:46 -0500

    Make small modifications to the "reddit search" project

---
+++ frontend.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import praw_stuff
+
+st.markdown(
+    """
+    <style>
+    @font-face {
+        font-family: 'CustomFont';
+        src: url('fonts/custom_font.otf') format('opentype');
+    }
+    html, body, [class*="css"]  {
+        font-family: 'CustomFont';
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+def clean_subreddits(subreddits):
+    """
+    when the user types a list of subreddits, 
+    remove commas and 'r/' prefix
+    """
+    cleaned = []
+    for sub in subreddits:
+        if ',' in sub:
+            sub = sub.replace(',', '')
+            sub = sub.strip()
+        if sub.startswith('r/') or sub.startswith('R/'):
+            sub = sub[2:]
+        if len(sub) > 0:
+            cleaned.append(sub.lower())
+    return cleaned
+
+
+
+if __name__ == '__main__':
+    with st.sidebar:
+        st.subheader('App created by Brian Salkas')
+        st.write('Got any ideas to improve this app?  Open an issue [here](%s)' % 'https://github.com/brianSalk/search-reddit/issues')
+        st.write('If you are a programmer, send me a pull request [here](%s)' % 'https://github.com/brianSalk/search-reddit/pulls')
+    title_text = 'reddit regex'.title()
+    st.markdown('<h1 style="color: #FF4500;">' + title_text + '</h1>',unsafe_allow_html=True)
+    st.markdown('<h2>It\'s like grep for reddit :-)</h2>', unsafe_allow_html=True)
+
+    col1,col2 = st.columns(2)
+    with col2:
+            sort_by = st.radio(
+                    'sort submissions by:',
+                    ['new','hot', 'controversial'],
+                    index=0,
+                    horizontal=True
+                    )
+            limit = sub_limit = st.number_input(
+                    'max number of submissions to search per subreddit',
+                    0,
+                    None,
+                    value=100,
+                    step=1,
+                    )
+    with st.form('form'):
+        subreddits =    st.text_input('space-seperated list of subreddits', placeholder='Enter subreddits here: Ex. programminghumor animalrights jokes').split()
+        search_string = st.text_input('word, phrase, or regex', placeholder="Ex. gr[ea]y matches 'grey' and 'gray'")
+        
+        # remove invalid characters and r/ prefix
+        subreddits = clean_subreddits(subreddits)
+        
+
+        is_case_sensitive = st.checkbox('make search case sensitive')
+        is_whole_word = st.checkbox('search as whole word', help="Check this box to avoid matching for parts of words.  For example, if you are searching 'ed' but do not want to show matches for 'helped' or 'education'")
+        is_regex = st.checkbox('use regex', help="[click here](https://www.dataquest.io/wp-content/uploads/2019/03/python-regular-expressions-cheat-sheet.pdf) for a cheat-sheet")
+        include_comments = st.checkbox('search in submission comments', value=True)
+        
+        submitted = st.form_submit_button('Search in subreddits')
+
+        if submitted:
+            with st.spinner(f'finding occurances of {search_string} in {" ".join(subreddits)}...'):
+                praw_stuff.search_in_subreddits(
+                        search_string, 
+                        subreddits, 
+                        ignore_case = not is_case_sensitive, 
+                        whole_word=is_whole_word, 
+                        use_regex=is_regex, 
+                        include_comments=include_comments,
+                        limit=limit,
+                        sort_by=sort_by
+                        )
---
+++ praw_stuff.py
@@ -0,0 +1,132 @@
+import re
+import praw
+import os
+import streamlit as st
+
+reddit = praw.Reddit(
+        client_id = st.secrets['CID'],
+        client_secret = st.secrets['SECRET'],
+        user_agent = 'Myyyy reddit scraper for a string of text',
+        username = st.secrets['REDDIT_USER_NAME'],
+        password = st.secrets['REDDIT_PASSWORD']
+        )
+
+def get_bad_subreddits(subreddits):
+    nonexistant = []
+    for subreddit in subreddits:
+        try:
+            reddit.subreddit(subreddit).new(limit=None)
+        except Exception as e:
+            nonexistant.append(subreddit)
+    return nonexistant
+
+
+def squeeze_spaces(s):
+    s = s.replace('\n', ' ')
+    s = ' '.join(s.split())
+    return s
+
+def compile_word_re(s, ):
+    return re.compile(fr'\b{s}\b')
+
+
+def find_string_in_subreddit(string, subreddit_name, ignore_case=True, whole_word=False, use_regex=False, include_comments=True, limit=None, sort_by='new'):
+    # get each submission within the subreddit
+    try:
+        if sort_by == 'new':
+            submissions = reddit.subreddit(subreddit_name).new(limit=limit)
+        elif sort_by == 'hot':
+            submissions = reddit.subreddit(subreddit_name).hot(limit=limit)
+        elif sort_by == 'controversial':
+            submissions = reddit.subreddit(subreddit_name).controversial(limit=limit)
+        for submission in submissions:
+            found_in_post = False
+            found_in_comments_count = 0
+            url = 'www.reddit.com' + submission.permalink
+            print('---------------------------',url)
+            title = submission.title
+            selftext = submission.selftext
+            regex = ''
+            if whole_word:
+                regex = compile_word_re(string)
+            elif use_regex:
+                regex = re.compile(string)
+            title = squeeze_spaces(title)
+            selftext = squeeze_spaces(selftext)
+            if ignore_case:
+                title = title.lower()
+                selftext = selftext.lower()
+                string = string.lower()
+            print('submission: ', title)
+            # check if string is in title or submission selftext
+            if whole_word:
+                if regex.search(title) or regex.search(selftext):
+                    found_in_post = True
+            elif use_regex:
+                if regex.search(title) or regex.search(selftext):
+                    found_in_post = True
+            else:
+                if string in title or string in selftext:
+                    found_in_post = True
+
+            comment_forest = None
+            # SEARCH COMMENTS
+            if include_comments:
+                submission.comments.replace_more(limit=None)
+                print('comment searched')
+                for comment in submission.comments.list():
+                    print('comment')
+                    comment_body = comment.body
+                    comment_body = squeeze_spaces(comment_body)
+                    if ignore_case:
+                        comment_body = comment_body.lower()
+                    if use_regex or whole_word:
+                        if regex.search(comment_body):
+                            found_in_comments_count += 1
+                    else:
+                        if string in comment_body:
+                            found_in_comments_count += 1
+            comment = ''
+            if found_in_comments_count == 1:
+                comment = f'found in {found_in_comments_count} comment'
+            if found_in_comments_count > 1:
+                comment = f'found in {found_in_comments_count} comments'
+
+            if found_in_post or found_in_comments_count > 1:
+                #st.page_link(page='https://'+url, label=title)
+                #st.write(comment)
+                st.markdown(f'[{title}](%s) {comment}' % ('https://' + url))
+    except Exception as e:
+        st.write(f':red[{subreddit_name} not found, check your spelling]')
+
+
+def search_in_subreddits(string, subreddit_names, ignore_case=True, whole_word=False, use_regex=False, include_comments=True, limit=None, sort_by='new'):
+    if not string or not subreddit_names:
+        st.write(':red[search string and subreddits cannot be blank]')
+        return
+    st.spinner('scraping reddit')
+    for subreddit_name in subreddit_names:
+        st.markdown("<h2 style='color: blue;'>" + f"Results for '{string}' in:" + "</h2>", unsafe_allow_html=True)
+        st.markdown("<h3 style='color: blue;'>" + "r/" + subreddit_name + "</h3>", unsafe_allow_html=True)
+        subreddit_name = subreddit_name.lower()
+        try:
+            find_string_in_subreddit(
+                    string, 
+                    subreddit_name, 
+                    ignore_case=ignore_case, 
+                    whole_word=whole_word, 
+                    use_regex=use_regex, 
+                    include_comments=include_comments,
+                    limit=limit,
+                    sort_by=sort_by
+                    )    
+        except Exception as e:
+            st.write('red:[One or more subreddits not found]')
+
+
+
+
+if __name__ == "__main__":
+    subreddit = input('enter subreddit name:')
+    string = input('enter search string:')
+    find_string_in_subreddit(string, subreddit)
